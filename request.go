7bbdd1f4745a7d54807006a25887f76ab4e15338
Manuel Zaragoza <jmanueldev@gmail.com>
2025-03-02 15:13:31 -0800

    Defined an interface for building endpoint urls

--- request.go
+++ request.go
@@ -8,3 +8,7 @@ import (
 func requestUrl(path string, v url.Values) string {
 	return fmt.Sprintf("%s?%s", path, v.Encode())
 }
+
+type requestBuilder interface {
+	endpoint(path string, v url.Values) string
+}
